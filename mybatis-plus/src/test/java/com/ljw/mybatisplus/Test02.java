006f369389bc8a2ee64c04f3cc6c484791649cad
luojianwen <1352224809@qq.com>
2023-03-31 22:42:02 +0800

    Merge branch 'master' of github.com:it-Irving/test_idea
     Conflicts:
    	mybatis-plus/src/test/java/com/ljw/mybatisplus/Test01.java
    	mybatis-plus/src/test/java/com/ljw/mybatisplus/Test02.java
    和并分支

--- mybatis-plus/src/test/java/com/ljw/mybatisplus/Test02.java
+++ mybatis-plus/src/test/java/com/ljw/mybatisplus/Test02.java
@@ -4,6 +4,8 @@ public class Test02 {
     public static void main(String[] args) {
         System.out.println("11111111111111111111");
         System.out.println("5555");
+        System.out.println("5555");
+        System.out.println("5555");
     }
 
 
